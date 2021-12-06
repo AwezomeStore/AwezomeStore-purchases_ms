e62e32947c3ccafc85c99f235f0ac5c22f3b37cd
jebalseroo <38665510+jebalseroo@users.noreply.github.com>
2021-12-06 18:49:20 -0500

    Update database.js

--- AwezomeStore-purchases_ms/src/database.js
+++ AwezomeStore-purchases_ms/src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://bd/Mydatabase', {
+mongoose.connect('mongodb://bd/awezomestore_purchases_db', {
     
 })
     .then(db => console.log('Db is coneted to', db.Connection.host))
